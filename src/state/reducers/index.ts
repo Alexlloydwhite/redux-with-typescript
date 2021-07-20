2ad737463bc51aa8ecbea9da81c842428126ccc0
Alex White <alexlloydwhite@gmail.com>
2021-07-20 10:34:50 -0500

    combining reducers

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import bankReducer from  '../reducers/bankReducer';
+
+const reducers = combineReducers({
+    bank: bankReducer
+});
+
+export default reducers;
